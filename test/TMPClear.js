06b7b263a03685ad30ef5f80a861ca7c386a9551
Dhyey Moliya <dhyeymoliya@gmail.com>
2020-04-11 12:30:11 +0530

    Updated path of the pdf files

--- test/TMPClear.js
+++ test/TMPClear.js
@@ -17,8 +17,8 @@ function readFile(filePath) {
 function atest() { return new Promise((resolve, reject) => { setTimeout(() => { resolve() }, 10000) }) }
 
 (async function () {
-    const pdf1 = await readFile('./test/portrait-singlepage.pdf');
-    const pdf2 = await readFile('./test/portrait-multipage.pdf');
+    const pdf1 = await readFile('./portrait-singlepage.pdf');
+    const pdf2 = await readFile('./portrait-multipage.pdf');
     const result = await combinePDF([pdf1, pdf2]);
     await atest();
 }())
